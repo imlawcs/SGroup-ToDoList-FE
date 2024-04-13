6fc8dce4e9a2402c8a99558da0b655b02b7310ad
imlawcs <daolehanhnguyen@gmail.com>
2024-04-13 18:19:40 +0700

    final commit

--- main_home.js
+++ main_home.js
@@ -10,7 +10,7 @@ var blocked = []
 function render() {
     let items = todos.map(function (item, index) {
         return `
-        <div class="task-container" style="display: block;" draggable = "true">
+        <div class="task-container" id="${index}" style="display: block;" draggable = "true">
             <div class="task-content">
                 <div class="task-header">
                     <div style="display: flex; justify-content: space-between;">
@@ -27,18 +27,17 @@ function render() {
                 </div>
                 <div class="task-time">
                     <i class="fa-regular fa-clock"></i>
-                    <span>June 30, 2022</span>
+                    <span>${item.time}</span>
                 </div>
             </div>
         </div>
         `
     })
     containerItemTodo.innerHTML = items.join('');
-    console.log("123");
 
     let itemDoings = doings.map(function (item, index) {
         return `
-        <div class="task-container" style="display: block;" draggable = "true">
+        <div class="task-container" id="${index}" style="display: block;" draggable = "true">
             <div class="task-content">
                 <div class="task-header">
                     <div style="display: flex; justify-content: space-between;">
@@ -55,7 +54,7 @@ function render() {
                 </div>
                 <div class="task-time">
                     <i class="fa-regular fa-clock"></i>
-                    <span>June 30, 2022</span>
+                    <span>${item.time}</span>
                 </div>
             </div>
         </div>
@@ -66,7 +65,7 @@ function render() {
 
     let itemCom = completeds.map(function (item, index) {
         return `
-        <div class="task-container" style="display: block;" draggable = "true">
+        <div class="task-container" id="${index}" style="display: block;" draggable = "true">
             <div class="task-content">
                 <div class="task-header">
                     <div style="display: flex; justify-content: space-between;">
@@ -83,7 +82,7 @@ function render() {
                 </div>
                 <div class="task-time">
                     <i class="fa-regular fa-clock"></i>
-                    <span>June 30, 2022</span>
+                    <span>${item.time}</span>
                 </div>
             </div>
         </div>
@@ -94,7 +93,7 @@ function render() {
 
     let itemBlocked = blocked.map(function (item, index) {
         return `
-        <div class="task-container" style="display: block;" draggable = "true">
+        <div class="task-container" id="${index}" style="display: block;" draggable = "true">
             <div class="task-content">
                 <div class="task-header">
                     <div style="display: flex; justify-content: space-between;">
@@ -111,27 +110,27 @@ function render() {
                 </div>
                 <div class="task-time">
                     <i class="fa-regular fa-clock"></i>
-                    <span>June 30, 2022</span>
+                    <span>${item.time}</span>
                 </div>
             </div>
         </div>
         `
     })
     containerItemBlocked.innerHTML = itemBlocked.join('');
+    showCount();
 }
-
 
 var containerItemTodo = document.querySelector('.containerItemTodo');
 var containerItemDoing = document.querySelector('.containerItemDoing');
 var containerItemCompleted = document.querySelector('.containerItemCompleted');
 var containerItemBlocked = document.querySelector('.containerItemBlocked');
 
-//set todo array from local storage
+//Lấy các mảng được lưu trong localstorage hiển thị ra màn hình
 if(localStorage.getItem('todos')){
     todos = JSON.parse(localStorage.getItem('todos'));
     let items = todos.map(render);
     containerItemTodo.innerHTML = items.join('');
-    // count_todo.innerHTML = arr_todo.length;
+    console.log(containerItemTodo);
 }
 
 if (localStorage.getItem('doings')) {
@@ -150,7 +149,28 @@ if (localStorage.getItem('blocked')) {
     containerItemBlocked.innerHTML = items.join('');
 }
 
-//validate va luu new task vao local storage
+//Lấy thời gian thực
+function getCurrentTime(){
+    let dt = new Date();
+    let month = dt.toLocaleString('en-US', { month: 'long' });
+    let day = dt.getDate();
+    let year = dt.getFullYear();
+    return `${month} ${day}, ${year}`;
+}
+
+//Đếm số task ở mỗi cột
+function showCount(){
+    let todoCount = document.querySelector(".todo-count")
+    todoCount.innerHTML = todos.length
+    let doingCount = document.querySelector(".doing-count")
+    doingCount.innerHTML = doings.length
+    let completedCount = document.querySelector(".completed-count")
+    completedCount.innerHTML = completeds.length
+    let blockedCount = document.querySelector(".blocked-count")
+    blockedCount.innerHTML = blocked.length
+}
+
+//validate và lưu task mới vào local storage
 var categoryInput = document.querySelector('.category-input');
 var titleInput = document.querySelector('.title-input');
 var contentInput = document.querySelector('.content-input');
@@ -183,13 +203,16 @@ function onCreate() {
         todos.push({
             category: categoryValue,
             title: titleValue,
-            content: contentValue
+            content: contentValue,
+            time: getCurrentTime()
         })
         localStorage.setItem('todos', JSON.stringify(todos));
         newContainer.classList.remove('active')
         render();
         location.reload(true);
     }
+    showCount();
+    console.log(getCurrentTime());
 }
 
 //hien thi man hinh nhap new task
@@ -216,13 +239,6 @@ exitadd.addEventListener('click', function () {
     newContainer.classList.toggle('active', false);
 })
 
-// var editTask = document.querySelector('.edit');
-// var editContainer = document.querySelector('.edit-container');
-// var editContent = document.querySelector('.edit-content');
-// editTask.addEventListener('click', function () {
-//     editContainer.classList.toggle('active', true)
-// })
-
 //edit
 var editContent = document.querySelector('.edit-content');
 var editContainer = document.querySelector('.edit-container');
@@ -237,6 +253,10 @@ var checkBoxTodo = document.querySelector('.checkBoxTodo')
 var checkBoxDoing = document.querySelector('.checkBoxDoing')
 var checkBoxCompleted = document.querySelector('.checkBoxCompleted')
 var checkBoxBlocked = document.querySelector('.checkBoxBlocked')
+
+
+var checkBoxTemp = document.querySelector('.checkBoxTodo');
+
 function onEdit(id, type) {
     idd = id;
     typee = type;
@@ -248,19 +268,26 @@ function onEdit(id, type) {
     console.log(array[id].category)
     titleEdit.value = array[id].title;
     contentEdit.value = array[id].content;
-    if (type == 'todos') checkBoxTodo.checked = true;
+    if (type == 'todos') {
+        checkBoxTodo.checked = true;
+        checkBoxTemp = checkBoxTodo
+    }
     else if (type == 'doings') {
         checkBoxDoing.checked = true;
+        checkBoxTemp = checkBoxDoing
     }
     else if (type == 'completeds') {
         checkBoxCompleted.checked = true;
+        checkBoxTemp = checkBoxCompleted
     }
     else if (type == 'blocked') {
         checkBoxBlocked.checked = true;
+        checkBoxTemp = checkBoxBlocked
     }
 
     editContainer.classList.toggle('active')
     //xuat hien ra man hinh
+    showCount();
 }
 
 //mot checkbox duoc chon trong mot thoi diem
@@ -284,8 +311,6 @@ var submitEdit = document.querySelector('.edit-submit')
 
 var checkSubmitEdit = false;
 
-var checkBoxTemp = document.querySelector('.checkBoxTodo');
-
 document.querySelectorAll('.ch').forEach(function (item) {
     item.addEventListener('click', function () {
         checkBoxTemp = item;
@@ -293,66 +318,84 @@ document.querySelectorAll('.ch').forEach(function (item) {
 })
 
 submitEdit.addEventListener('click', function () {
-    console.log('hello')
-    checkSubmitEdit = true;
-    console.log(idd, typee)
-    var array = window[typee];
-    array[idd].category = categoryEdit.value;
-    array[idd].title = titleEdit.value;
-    array[idd].content = contentEdit.value;
-    var itemAdd = array[idd];
-    console.log(itemAdd)
-    array.splice(idd, 1);
+    var check = true;
+    if (categoryEdit.value == '') {
+        check = false;
+        categoryEdit.classList.add('error');
+    }
+    else categoryEdit.classList.remove('error');
+    if (titleEdit.value == '') {
+        check = false;
+        titleEdit.classList.add('error')
+    }
+    else titleEdit.classList.remove('error');
+    if (contentEdit.value == '') {
+        check = false;
+        contentEdit.classList.add('error')
+    }
+    else contentEdit.classList.remove('error');
+    if (check == true) { 
+        checkSubmitEdit = true;
+        console.log(idd, typee)
+        var array = window[typee];
+        array[idd].category = categoryEdit.value;
+        array[idd].title = titleEdit.value;
+        array[idd].content = contentEdit.value;
+        var itemAdd = array[idd];
+        array.splice(idd, 1);
 
-    if (checkBoxTemp.parentNode.querySelector('.lable').textContent == 'Todo') {
-        todos.push(itemAdd)
+        if (checkBoxTemp.parentNode.querySelector('.lable').textContent == 'Todo') {
+            todos.push(itemAdd)
+            localStorage.setItem('todos', JSON.stringify(todos));
+            checkBoxTodo.checked = false;
+            checkBoxDoing.checked = false;
+            checkBoxCompleted.checked = false;
+            checkBoxBlocked.checked = false;
+            render()
+        }
+
+        if (checkBoxTemp.parentNode.querySelector('.lable').textContent == 'Doing') {
+            doings.push(itemAdd)
+            localStorage.setItem('doings', JSON.stringify(doings));
+            checkBoxTodo.checked = false;
+            checkBoxDoing.checked = false;
+            checkBoxCompleted.checked = false;
+            checkBoxBlocked.checked = false;
+            render()
+        }
+
+        if (checkBoxTemp.parentNode.querySelector('.lable').textContent == 'Completed') {
+            completeds.push(itemAdd)
+            localStorage.setItem('completeds', JSON.stringify(completeds));
+            checkBoxTodo.checked = false;
+            checkBoxDoing.checked = false;
+            checkBoxCompleted.checked = false;
+            checkBoxBlocked.checked = false;
+            render()
+        }
+        if (checkBoxTemp.parentNode.querySelector('.lable').textContent == 'Blocked') {
+            blocked.push(itemAdd)
+            localStorage.setItem('blocked', JSON.stringify(blocked));
+            checkBoxTodo.checked = false;
+            checkBoxDoing.checked = false;
+            checkBoxCompleted.checked = false;
+            checkBoxBlocked.checked = false;
+            render()
+        }
+
         localStorage.setItem('todos', JSON.stringify(todos));
-        checkBoxTodo.checked = false;
-        checkBoxDoing.checked = false;
-        checkBoxCompleted.checked = false;
-        checkBoxBlocked.checked = false;
-        render()
-    }
-
-    if (checkBoxTemp.parentNode.querySelector('.lable').textContent == 'Doing') {
-        doings.push(itemAdd)
         localStorage.setItem('doings', JSON.stringify(doings));
-        checkBoxTodo.checked = false;
-        checkBoxDoing.checked = false;
-        checkBoxCompleted.checked = false;
-        checkBoxBlocked.checked = false;
-        render()
-    }
-
-    if (checkBoxTemp.parentNode.querySelector('.lable').textContent == 'Completed') {
-        completeds.push(itemAdd)
         localStorage.setItem('completeds', JSON.stringify(completeds));
-        checkBoxTodo.checked = false;
-        checkBoxDoing.checked = false;
-        checkBoxCompleted.checked = false;
-        checkBoxBlocked.checked = false;
-        render()
-    }
-    if (checkBoxTemp.parentNode.querySelector('.lable').textContent == 'Blocked') {
-        blocked.push(itemAdd)
         localStorage.setItem('blocked', JSON.stringify(blocked));
-        checkBoxTodo.checked = false;
-        checkBoxDoing.checked = false;
-        checkBoxCompleted.checked = false;
-        checkBoxBlocked.checked = false;
-        render()
-    }
-
-    localStorage.setItem('todos', JSON.stringify(todos));
-    localStorage.setItem('doings', JSON.stringify(doings));
-    localStorage.setItem('completeds', JSON.stringify(completeds));
-    localStorage.setItem('blocked', JSON.stringify(blocked));
-  
-    // localStorage.setItem(type, JSON.stringify(array)); 
-    editContainer.classList.toggle('active')
-    render();
+    
+        // localStorage.setItem(type, JSON.stringify(array)); 
+        editContainer.classList.toggle('active')
+        render();
+        location.reload(true);
+    }    
 })
 
+//exit edit
 var exit = document.querySelector('.exit');
 
 exit.addEventListener('click', function () {
@@ -371,82 +414,178 @@ editContent.addEventListener('click', function (event) {
     event.stopPropagation()
 })
 
+//xóa task
 function onDelete(id, type) {
     var array = window[type];
     array.splice(id, 1);
-    console.log('ne', array)
     localStorage.setItem('todos', JSON.stringify(todos));
     localStorage.setItem('doings', JSON.stringify(doings));
     localStorage.setItem('completeds', JSON.stringify(completeds));
     localStorage.setItem('blocked', JSON.stringify(blocked));
     render();
+    showCount();
+    location.reload(true);
 }
 
 //lay du lieu tu local storage in ra man hinh ngay tu ban dau
 render();
 
-//drag drop
 
-// const todos = document.querySelectorAll(".todo");
+//drag drop
 const taskContainer = document.querySelectorAll(".task-container");
-// const all_status = document.querySelectorAll(".status");
 const list = document.querySelectorAll(".list");
 const containerItem = document.querySelectorAll(".containerItem");
 let draggableTodo = null;
+var parent, idItem;
 
 taskContainer.forEach((item) => {
-  item.addEventListener("dragstart", dragStart);
-  item.addEventListener("dragend", dragEnd);
+    item.addEventListener("dragstart", dragStart);
+    item.addEventListener("dragend", dragEnd);
 });
 
 function dragStart() {
-  draggableTodo = this;
-  setTimeout(() => {
-    this.style.display = "none";
-  }, 0);
-  console.log("dragStart");
+    draggableTodo = this;
+    setTimeout(() => {
+        this.style.display = "none";
+    }, 0);
+    parent = this.parentElement;
+    idItem = this.id;
+    console.log("dragStart");
 }
 
 function dragEnd() {
-  draggableTodo = null;
-  setTimeout(() => {
-    this.style.display = "block";
-  }, 0);
-  console.log("dragEnd");
-}
+    draggableTodo = null;
+    setTimeout(() => {
+        this.style.display = "block";
+    }, 0);
+    let parentElement = this.parentElement;
+    var task = parentElement.querySelectorAll('.task-container');
+    
+    //push vao mang moi
+    //thay doi id
+    let item;
+    item = {
+        category: this.querySelector(".topic").textContent,
+        title: this.querySelector(".task-header>p").textContent,
+        content: this.querySelector(".task p").textContent
+    }
+    if(parentElement.id == 1) {
+        todos.push(item)
+        let i = 0;
+        // Duyệt qua từng phần tử con và làm điều gì đó
+        localStorage.setItem('todos', JSON.stringify(todos));
+        task.forEach(function(item) {
+            item.id = i++;
+        })
+    }
+    let i;
+    if(parentElement.id == 2) {
+        doings.push(item)
+        i = 0;
+        // Duyệt qua từng phần tử con và làm điều gì đó
+        task.forEach(function(item) {
+            item.id = i;
+            i++;
+        })
+        localStorage.setItem('doings', JSON.stringify(doings));
+    }
 
-// list.forEach((list) => {
-//   list.addEventListener("dragover", dragOver);
-//   list.addEventListener("dragenter", dragEnter);
-//   list.addEventListener("dragleave", dragLeave);
-//   list.addEventListener("drop", dragDrop);
-// });
+    if(parentElement.id == 3) {
+        completeds.push(item)
+        i = 0;
+        // Duyệt qua từng phần tử con và làm điều gì đó
+        task.forEach(function(item) {
+            item.id = i++;
+        })
+        localStorage.setItem('completeds', JSON.stringify(completeds));
+    }
+
+    if(parentElement.id == 4) {
+        blocked.push(item)
+        i = 0;
+        // Duyệt qua từng phần tử con và làm điều gì đó
+        task.forEach(function(item) {
+            item.id = i++;
+        })
+        localStorage.setItem('blocked', JSON.stringify(blocked));
+    }
+
+    var task0 = parent.querySelectorAll('.task-container');
+    //clear o mang cu
+    if(parent.id == 1) {
+        let arr = todos;
+        arr.splice(idItem, 1);
+        i = 0;
+        // Duyệt qua từng phần tử con và làm điều gì đó
+        task0.forEach(function(item) {
+            item.id = i++;
+        })
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }
+
+    if(parent.id == 2) {
+        let arr = doings;
+        arr.splice(idItem, 1);
+        let i = 0;
+        // Duyệt qua từng phần tử con và làm điều gì đó
+        task0.forEach(function(item) {
+            item.id = i++;
+        })
+        localStorage.setItem('doings', JSON.stringify(doings));
+    }
+
+    if(parent.id == 3) {
+        let arr = completeds;
+        arr.splice(idItem, 1);
+        let i = 0;
+        // Duyệt qua từng phần tử con và làm điều gì đó
+        task0.forEach(function(item) {
+            item.id = i++;
+        })
+        localStorage.setItem('completeds', JSON.stringify(completeds));
+    }
+
+    if(parent.id == 4) {
+        let arr = blocked;
+        arr.splice(idItem, 1);
+        let i = 0;
+        // Duyệt qua từng phần tử con và làm điều gì đó
+        task0.forEach(function(item) {
+            item.id = i++;
+        })
+        localStorage.setItem('blocked', JSON.stringify(blocked));
+    }
+    showCount();
+    location.reload(true);
+    console.log("dragEnd");
+}
 
 containerItem.forEach((containerItem) => {
     containerItem.addEventListener("dragover", dragOver);
     containerItem.addEventListener("dragenter", dragEnter);
     containerItem.addEventListener("dragleave", dragLeave);
     containerItem.addEventListener("drop", dragDrop);
-  });
+});
 
 function dragOver(e) {
   e.preventDefault();
-  //   console.log("dragOver");
+    console.log("dragOver");
 }
 
 function dragEnter() {
-  this.style.border = "1px dashed #ccc";
-  console.log("dragEnter");
+    this.style.border = "1px dashed #ccc";
+    console.log("dragEnter");
 }
 
 function dragLeave() {
-  this.style.border = "none";
-  console.log("dragLeave");
+    this.style.border = "none";
+    console.log("dragLeave");
 }
 
 function dragDrop() {
-  this.style.border = "none";
-  this.appendChild(draggableTodo);
-  console.log("dropped");
+    this.style.border = "none";
+    this.appendChild(draggableTodo);
+    console.log("dropped");
 }
-// render();
+
+
